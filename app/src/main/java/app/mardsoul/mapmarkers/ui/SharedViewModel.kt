bba070b930807329c4fcada366e161b0290587df
Aleksei Laptev <mar.d.soul.app@gmail.com>
2022-10-18 20:57:39 +0300

    add Navigator.kt, add function clearPlaces(), add toast message when add place, add longClickListener to googleMap

--- app/src/main/java/app/mardsoul/mapmarkers/ui/SharedViewModel.kt
+++ app/src/main/java/app/mardsoul/mapmarkers/ui/SharedViewModel.kt
@@ -3,12 +3,29 @@ package app.mardsoul.mapmarkers.ui
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import app.mardsoul.mapmarkers.domain.MarkerUseCase
+import app.mardsoul.mapmarkers.domain.Place
+import com.google.android.gms.maps.model.LatLng
+import kotlinx.coroutines.launch
 
 class SharedViewModel(
-    markerUseCase: MarkerUseCase
+    private val markerUseCase: MarkerUseCase
 ) : ViewModel() {
     val markerLiveData = markerUseCase.getPlaces().asLiveData()
+
+    fun addPlace(latLng: LatLng) {
+        val newPlace = Place(latLng = latLng)
+        viewModelScope.launch {
+            markerUseCase.addPlace(newPlace)
+        }
+    }
+
+    fun clearPlaces() {
+        viewModelScope.launch {
+            markerUseCase.clearPlacesList()
+        }
+    }
 }
 
 class SharedViewModelFactory(private val markerUseCase: MarkerUseCase) : ViewModelProvider.Factory {
--- app/src/main/java/app/mardsoul/mapmarkers/ui/map/MapsFragment.kt
+++ app/src/main/java/app/mardsoul/mapmarkers/ui/map/MapsFragment.kt
@@ -1,8 +1,13 @@
 package app.mardsoul.mapmarkers.ui.map
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.annotation.DrawableRes
+import androidx.core.content.res.ResourcesCompat
 import androidx.lifecycle.lifecycleScope
 import app.mardsoul.mapmarkers.R
 import app.mardsoul.mapmarkers.databinding.FragmentMapsBinding
@@ -10,7 +15,9 @@ import app.mardsoul.mapmarkers.domain.Place
 import app.mardsoul.mapmarkers.ui.BaseFragment
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.ktx.addMarker
 import com.google.maps.android.ktx.awaitMap
 import com.google.maps.android.ktx.awaitMapLoad
@@ -23,26 +30,54 @@ class MapsFragment : BaseFragment<FragmentMapsBinding>(FragmentMapsBinding::infl
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         lifecycleScope.launchWhenCreated {
-            googleMap = mapFragment.awaitMap().apply { awaitMapLoad() }
+            googleMap = mapFragment.awaitMap().apply {
+                awaitMapLoad()
+                setOnMapLongClickListener {
+                    viewModel.addPlace(it)
+                    showAddToast(it)
+                }
+            }
+            viewModel.markerLiveData.observe(viewLifecycleOwner) { refreshMarkers(it) }
         }
-        viewModel.markerLiveData.observe(viewLifecycleOwner) { refreshMarkers(it) }
+    }
+
+    private fun showAddToast(latLng: LatLng) {
+        val toastText =
+            getString(R.string.add_place_toast_text, latLng.latitude, latLng.longitude)
+        Toast.makeText(requireContext(), toastText, Toast.LENGTH_SHORT).show()
     }
 
     private fun refreshMarkers(placeList: List<Place>) {
+        googleMap?.clear()
         placeList.forEach { addPlace(it) }
-        Toast.makeText(requireContext(), "Received new places list", Toast.LENGTH_SHORT).show()
     }
 
     private fun addPlace(place: Place) {
         googleMap?.addMarker {
             title(place.name)
             position(place.latLng)
-            icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_baseline_pin_24))
+            icon(bitmapDescriptorFromVector(R.drawable.ic_baseline_pin_24))
         }
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance() = MapsFragment()
+    /**
+     * Taken from ApiDemos on GitHub:
+     * https://github.com/googlemaps/android-samples/blob/master/ApiDemos/kotlin/app/src/main/java/com/example/kotlindemos/MarkerDemoActivity.kt
+     */
+    private fun bitmapDescriptorFromVector(@DrawableRes vectorResId: Int): BitmapDescriptor {
+        val vectorDrawable = ResourcesCompat.getDrawable(resources, vectorResId, null)
+        if (vectorDrawable == null) {
+            Log.e("BitmapHelper", "Resource not found")
+            return BitmapDescriptorFactory.defaultMarker()
+        }
+        val bitmap = Bitmap.createBitmap(
+            vectorDrawable.intrinsicWidth,
+            vectorDrawable.intrinsicHeight,
+            Bitmap.Config.ARGB_8888
+        )
+        val canvas = Canvas(bitmap)
+        vectorDrawable.setBounds(0, 0, canvas.width, canvas.height)
+        vectorDrawable.draw(canvas)
+        return BitmapDescriptorFactory.fromBitmap(bitmap)
     }
 }
